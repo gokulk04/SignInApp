ba964eedcc36c883d2a7d432b9bde5a6298bf012
Gokul Kumarresen <gokulk@live.com>
2017-01-11 04:34:46 -0500

    initial commit

---
+++ app/src/main/java/com/example/gokul/signinapp_1_8/AdditionalInfoActivity.java
@@ -0,0 +1,35 @@
+package com.example.gokul.signinapp_1_8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class AdditionalInfoActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button btnNext2to3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additional_info);
+        btnNext2to3 = (Button) this.findViewById(R.id.btnNext2to3);
+        btnNext2to3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch(id){
+            case R.id.btnNext2to3:
+                Intent AddInfoToProfPic = new Intent(AdditionalInfoActivity.this, ProfilePictureActivity.class);
+                startActivity(AddInfoToProfPic);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gokul/signinapp_1_8/LogInActivity.java
@@ -0,0 +1,13 @@
+package com.example.gokul.signinapp_1_8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
